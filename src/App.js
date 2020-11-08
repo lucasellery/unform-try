c84b9bf0c54bca498e17fd225c754028239d4fb4
Your Name <olucasellery@gmail.com>
2020-11-08 14:11:33 -0300

    Test finished/keep gooing w/ docs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,85 @@
-import React from 'react';
-import './App.css';
-import{ Form } from '@unform/web';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import { Form } from "@unform/web";
+import { Scope } from "@unform/core";
+import * as Yup from "yup";
+
+import Input from "./components/Form/Input";
+
+const initialData = {
+  email: "lucas@gmail.com.br",
+  address: {
+    city: "Fortaleza",
+  },
+};
 
 function App() {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("O nome é obrigatório"),
+        email: Yup.string().email().required("O email é obrigatório"),
+        address: Yup.object().shape({
+          city: Yup.string()
+            .min('3', "No mínimo 3 caracteres")
+            .required("A cidade é obrigatória"),
+        }),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      console.log(data);
+      
+      formRef.current.setErrors({});
+
+      reset();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        err.inner.forEach((error) => {
+          errorMessages[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      formRef.current.setData({
+        name: 'Lucas Arruda',
+        email: 'olucasellery@gmail.com',
+        address: {
+          city: 'Fortaleza',
+        }
+      })
+    }, 2000);
+  }, []);
+  
+
   return (
     <div className="App">
       <h1>Hello, world!</h1>
-      
-      <Form>
-        <Input name="email"/>
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input name="email" />
+
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="neighborhood" />
+          <Input name="city" />
+          <Input name="state" />
+          <Input name="number" />
+        </Scope>
+
+        <button type="submit">Enviar</button>
       </Form>
     </div>
   );
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,19 +1,25 @@
-import { React, useEffect} from "react";
+import { React, useEffect, useRef } from "react";
 import { useField } from "@unform/core";
 // useField API usada para conectar o Input com o Unform
 
-function Input({ name }) {
-  const { fieldName, registerField, defaultValue, error} = useField(name);
+function Input({ name, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
 
   useEffect(() => {
-
-  }, []);
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value", // prop que contém o valor do input dentro da ref
+    });
+  }, [fieldName, registerField]);
 
   return (
-  
-  <input 
-    onChange={e => e.target.value}
-  />)
+    <div>
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />{" "}
+      {error && <span style={{ color: "#f00" }}>{error}</span>}
+    </div>
+  );
 }
 
 export default Input;
